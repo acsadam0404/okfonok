83ca26d40d835c7706fdab23d874ceaf32be397e
Ács Ádám <acsadam0404@gmail.com>
2014-02-14 23:35:13 +0100

    registration mail sender

--- okfonok-bus-mail/src/main/java/hu/okfonok/RegistrationMailSender.groovy
+++ okfonok-bus-mail/src/main/java/hu/okfonok/RegistrationMailSender.groovy
@@ -1,5 +1,10 @@
 package hu.okfonok
 
+import javax.inject.Inject
+import javax.inject.Named
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException
 import org.springframework.mail.MailSender
 import org.springframework.mail.SimpleMailMessage
@@ -9,28 +14,28 @@ import org.springframework.mail.SimpleMailMessage
  * @author Ács Ádám
  *
  */
+@Named("registrationMailSender")
 class RegistrationMailSender {
-	private MailSender mailSender;
-	private SimpleMailMessage templateMessage;
-	
-	public void setMailSender(MailSender mailSender) {
-		this.mailSender = mailSender;
-	}
-
-	public void setTemplateMessage(SimpleMailMessage templateMessage) {
-		this.templateMessage = templateMessage;
-	}
+	private static final Logger log = LoggerFactory.getLogger(RegistrationMailSender.class)
+	 
+	@Inject
+	private MailSender mailSender
 
 	public void send() {
 
-		SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
-		msg.setTo("acsadam0404@gmail.com");
-		msg.setText("fuck that shit $mailSender");
-		try{
-			this.mailSender.send(msg);
-		}
-		catch(MailException ex) {
-			System.err.println(ex.getMessage());
+		SimpleMailMessage msg = new SimpleMailMessage();
+		
+		msg.setFrom("acsadam0404@gmail.com")
+		msg.setTo("acsadam0404@gmail.com")
+		msg.setText("okfonok")
+		msg.setSubject("okfonok")
+		
+		try {
+			mailSender.send(msg);
+			log.info("Sikeres üzenetküldés. Üzenet:\n ${msg.to}\n${msg.subject}\n${msg.text}");
+		} 
+		catch (MailException mex) {
+			log.error("Sikertelen üzenet küldés regisztrációkor. Üzenet:\n ${msg.from}\n${msg.to}\n${msg.subject}\n${msg.text}", mex);
 		}
 	}
 }
