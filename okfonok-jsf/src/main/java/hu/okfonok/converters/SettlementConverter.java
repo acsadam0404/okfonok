dcd725e25ddd65b775c37101f28b82dd85ecbd88
Ács Ádám <acsadam0404@gmail.com>
2014-05-16 16:50:50 +0200

    workspace rendberakása

---
+++ okfonok-jsf/src/main/java/hu/okfonok/converters/SettlementConverter.java
@@ -0,0 +1,51 @@
+package hu.okfonok.converters;
+
+import hu.okfonok.entities.Settlement;
+import hu.okfonok.services.SettlementService;
+import hu.okfonok.utils.ServiceLocator;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * 
+ * @author Ács Ádám
+ *
+ */
+@Named("settlementConverter")
+@Scope("singleton")
+public class SettlementConverter implements Converter {
+	private SettlementService service;
+
+	public SettlementService getService() {
+		if (service == null) {
+			service = ServiceLocator.getBean(SettlementService.class);
+		}
+		return service;
+	}
+
+	@Override
+	public Settlement getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		if (StringUtils.isBlank(value)) {
+			return null;
+		}
+
+		return getService().findBySettlement(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		if (value == null || !(value instanceof Settlement)) {
+			return "";
+		}
+		Settlement Settlement = (Settlement) value;
+		return String.valueOf(Settlement.getSettlement());
+	}
+}
