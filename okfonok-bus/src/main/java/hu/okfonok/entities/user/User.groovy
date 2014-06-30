e110affd6240e544da3c94438ebeb9f1f6a3df4b
acsadam0404 <acsadam0404@gmail.com>
2014-06-30 16:11:59 +0200

    skillek, taglibek és advertisements

--- okfonok-bus/src/main/java/hu/okfonok/entities/user/User.groovy
+++ okfonok-bus/src/main/java/hu/okfonok/entities/user/User.groovy
@@ -23,36 +23,37 @@ import javax.validation.constraints.Size
  */
 @Entity
 class User extends BaseEntity implements Serializable{
-	
+
 	@Column(unique = true)
 	@NotNull
 	@Size(min=4, max=20)
 	String userName
-	
+
 	@NotNull
 	@Size(min=4)
 	String password
-	
+
 	Boolean enabled = false
-	
+
 	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
 	@JoinTable(name = "User_Roles", joinColumns = [ @JoinColumn(name = "user_id", referencedColumnName="id") ], inverseJoinColumns = [ @JoinColumn(name = "role_id", referencedColumnName="id") ])
 	Set<Role> roles = new HashSet<Role>()
-	
+
 	@Embedded
 	Address address = new Address()
-	
+
 	@Embedded
 	Certification certification = new Certification()
-	
+
 	@Embedded
 	Profile profile = new Profile()
-	
+
 	String providerId
-	
+
 	@Embedded
 	Paypal paypal
-	
-	
 
+	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinTable(name = "User_Skills", joinColumns = [ @JoinColumn(name = "user_id", referencedColumnName="id") ], inverseJoinColumns = [ @JoinColumn(name = "skill_id", referencedColumnName="id") ])
+	Set<Skill> skills = new HashSet<Skill>()
 }
--- okfonok-jsf/src/main/java/hu/okfonok/beans/AdvertisementsBean.groovy
+++ okfonok-jsf/src/main/java/hu/okfonok/beans/AdvertisementsBean.groovy
@@ -1,40 +1,60 @@
 package hu.okfonok.beans;
 
 import hu.okfonok.entities.Advertisement
-import hu.okfonok.entities.JobCategory
 import hu.okfonok.services.AdvertisementService
 import hu.okfonok.services.JobCategoryService
-import hu.okfonok.utils.ServiceLocator;
+import hu.okfonok.utils.ServiceLocator
 
 import javax.faces.model.SelectItem
 
+import org.primefaces.context.RequestContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 
-@org.springframework.stereotype.Component("advertisementsBean")
+@org.springframework.stereotype.Component("adsBean")
 @Scope("view")
 class AdvertisementsBean implements Serializable {
-	@Autowired 
-	private AdvertisementService service;
-	@Autowired 
-	private JobCategoryService jcService;
+	@Autowired
+	private AdvertisementService service
+	@Autowired
+	private JobCategoryService jcService
 
-	private List<Advertisement> ads;
-	List<Advertisement> filteredAds;
+	private List<Advertisement> ads
+	List<Advertisement> filteredAds
+	Advertisement selected
 
 	List<Advertisement> getAds() {
 		if (!ads) {
 			ads = service.findAll()
 		}
+
+		//FIXME testadat
+		ads << new Advertisement(description: 'teszt leírás', jobTime: new Date(), expiration :new Date())
+		//FIXME vége
 		return ads
 	}
 
 	def getMainCategoryOptions() {
-		ServiceLocator.getBean(JobCategoryService.class).findAllMain() 
+		ServiceLocator.getBean(JobCategoryService.class).findAllMain()
 	}
-	
+
 	SelectItem[] getSubCategoryOptions() {
 		SelectItem[] options = new SelectItem[0] //TODO
 		return options
+	}
+
+	public void viewAd() {
+		Map options = [
+			modal: true
+			,draggable: false
+			,resizable: false
+			,contentHeight: 470
+			,contentWidth: 620
+			,width: 650
+			,height: 500
+		]
+
+//		RequestContext.getCurrentInstance().openDialog("fragments/index/viewAdDialog", options, null);
+		RequestContext.getCurrentInstance().openDialog("test");
 	}
 }
