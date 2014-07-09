cbdd329e54f0a91597decacd6d3e504972739767
acsadam0404 <acsadam0404@gmail.com>
2014-07-09 14:42:56 +0200

    stílusok

--- okfonok-jsf/src/main/java/hu/okfonok/beans/IndexBean.groovy
+++ okfonok-jsf/src/main/java/hu/okfonok/beans/IndexBean.groovy
@@ -5,8 +5,6 @@ import hu.okfonok.services.UserService
 
 import javax.annotation.PostConstruct
 
-import org.primefaces.push.PushContext
-import org.primefaces.push.PushContextFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 
@@ -18,31 +16,8 @@ import org.springframework.context.annotation.Scope
 @org.springframework.stereotype.Component("indexBean")
 @Scope("view")
 class IndexBean implements Serializable{
-	boolean registrationFormVisible
-
-	List<User> employees
-	User emp1
-	User emp2
-	int currentIndex
 
 	@Autowired
 	private UserService userService
 
-	@PostConstruct
-	void init() {
-		employees = userService.findAll()
-		emp1 = employees[0]
-		emp2 = employees[1]
-	}
-
-
-	void pushEmployees() {
-		currentIndex = currentIndex < (employees.size() - 1) ? currentIndex + 1 : 0
-		emp1 = employees[currentIndex]
-		currentIndex = currentIndex < (employees.size() - 1) ? currentIndex + 1 : 0
-		emp2 = employees[currentIndex]
-
-		PushContext pushContext = PushContextFactory.getDefault().getPushContext();
-		pushContext.push("/employees", "test")
-	}
 }
