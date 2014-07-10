d97a8a364f2cb227fee14370189a13e7d40c4f8b
acsadam0404 <acsadam0404@gmail.com>
2014-07-10 16:05:48 +0200

    calendar és timepicker I18N-esítése

---
+++ okfonok-jsf/src/main/webapp/resources/js/timepicker_hu.js
@@ -0,0 +1,8 @@
+PrimeFacesExt.locales.TimePicker['hu'] = {
+	hourText : 'Óra',
+	minuteText : 'Perc',
+	amPmText : [ '', '' ],
+	closeButtonText : 'Kész',
+	nowButtonText : 'Most',
+	deselectButtonText : 'Töröl'
+};
