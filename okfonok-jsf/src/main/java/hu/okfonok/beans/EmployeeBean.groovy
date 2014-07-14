2660cc70f183fb0cf4edb514185dd66f299e3022
acsadam0404 <acsadam0404@gmail.com>
2014-07-14 15:55:06 +0200

    EmployeeBean felesleges metódusok törlése

--- okfonok-jsf/src/main/java/hu/okfonok/beans/EmployeeBean.groovy
+++ okfonok-jsf/src/main/java/hu/okfonok/beans/EmployeeBean.groovy
@@ -9,27 +9,4 @@ import org.springframework.context.annotation.Scope
 @org.springframework.stereotype.Component("employeeBean")
 @Scope("view")
 class EmployeeBean implements Serializable {
-	private List<User> users;
-
-	private User selectedUser;
-
-	public EmployeeBean() {
-		users = new ArrayList<User>();
-
-		users.add(new User(userName: "tesztuser1" ));
-		users.add(new User(userName: "tesztuser2" ));
-		users.add(new User(userName: "tesztuser3" ));
-	}
-
-	public List<User> getUsers() {
-		return users;
-	}
-
-	public User getSelectedUser() {
-		return selectedUser;
-	}
-
-	public void setSelectedUser(User selectedUser) {
-		this.selectedUser = selectedUser;
-	}
 }
