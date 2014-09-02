2717572d8e1ccde1417f4c02caf78f86102db4cf
acsadam0404 <acsadam0404@gmail.com>
2014-09-02 08:37:46 +0200

    offer

---
+++ okfonok-bus/src/main/java/hu/okfonok/entities/Offer.groovy
@@ -0,0 +1,19 @@
+package hu.okfonok.entities;
+
+import groovy.transform.EqualsAndHashCode
+import hu.okfonok.entities.user.User
+
+import javax.persistence.Entity
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "offer")
+@EqualsAndHashCode
+class Offer extends BaseEntity{
+	@ManyToOne
+	Advertisement ad
+	
+	@ManyToOne
+	User user
+}
