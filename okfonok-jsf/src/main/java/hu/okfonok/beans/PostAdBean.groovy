586e53c51099811b370a1fd7569e2d310bff7e2a
Mester Tamás <mesttamas@gmail.com>
2014-09-08 19:54:38 +0200

    hirdetés feladás naptár issue58

--- okfonok-jsf/src/main/java/hu/okfonok/beans/PostAdBean.groovy
+++ okfonok-jsf/src/main/java/hu/okfonok/beans/PostAdBean.groovy
@@ -5,7 +5,16 @@ import hu.okfonok.services.AdvertisementService
 import hu.okfonok.services.JobCategoryService
 import hu.okfonok.services.UserService
 
+import javax.faces.application.FacesMessage
+import javax.faces.event.ActionEvent
+
 import org.primefaces.context.RequestContext
+import org.primefaces.event.ScheduleEntryMoveEvent
+import org.primefaces.event.ScheduleEntryResizeEvent
+import org.primefaces.event.SelectEvent
+import org.primefaces.model.DefaultScheduleEvent
+import org.primefaces.model.ScheduleEvent
+import org.primefaces.model.ScheduleModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 
@@ -18,27 +27,69 @@ import org.springframework.context.annotation.Scope
 @Scope("session")
 class PostAdBean implements Serializable{
 	Advertisement ad = new Advertisement()
-	
+
 	@Autowired
 	private AdvertisementsBean adsBean
-	
-	@Autowired 
+
+	@Autowired
 	private AdvertisementService adService
-	@Autowired 
+	@Autowired
 	private JobCategoryService jcs
-	@Autowired 
+	@Autowired
 	private UserService userService
 	@Autowired
 	private UserBean userBean
-	
-	
-	
+
+	private ScheduleModel eventModel;
+	private ScheduleEvent event = new DefaultScheduleEvent();
+
 	String post() {
 		ad.user = userBean.user
 		adService.save(ad)
 		RequestContext.getCurrentInstance().execute("postadDialog.hide();")
 		ad = new Advertisement()
-		
+
 		return 'index.xhtml?faces-redirect=true'
+	}
+
+	public void onDateSelect(SelectEvent selectEvent) {
+		event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+	}
+
+	public void onEventMove(ScheduleEntryMoveEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	public void onEventResize(ScheduleEntryResizeEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	public ScheduleModel getEventModel() {
+		return eventModel;
+	}
+
+	public ScheduleEvent getEvent() {
+		return event;
+	}
+
+	public void setEvent(ScheduleEvent event) {
+		this.event = event;
+	}
+
+	public void onEventSelect(SelectEvent selectEvent) {
+		event = (ScheduleEvent) selectEvent.getObject();
+	}
+	
+	public void addEvent(ActionEvent actionEvent) {
+		if(event.getId() == null)
+			eventModel.addEvent(event);
+		else
+			eventModel.updateEvent(event);
+
+		event = new DefaultScheduleEvent();
 	}
 }
--- okfonok-jsf/src/main/java/hu/okfonok/beans/ScheduleBean.groovy
+++ okfonok-jsf/src/main/java/hu/okfonok/beans/ScheduleBean.groovy
@@ -1,65 +1,67 @@
-package hu.okfonok.beans;
+//package hu.okfonok.beans;
+//
+//import javax.faces.application.FacesMessage
+//import javax.faces.context.FacesContext
+//import javax.faces.event.ActionEvent
+//import javax.inject.Named
+//
+//import org.primefaces.event.ScheduleEntryMoveEvent
+//import org.primefaces.event.ScheduleEntryResizeEvent
+//import org.primefaces.event.SelectEvent
+//import org.primefaces.model.DefaultScheduleEvent
+//import org.primefaces.model.DefaultScheduleModel
+//import org.primefaces.model.ScheduleEvent
+//import org.primefaces.model.ScheduleModel
+//import org.springframework.context.annotation.Scope
+//
+///**
+// * 
+// * @author Ács Ádám
+// *
+// */
+//@org.springframework.stereotype.Component("scheduleBean")
+//@Scope("session")
+//class ScheduleBean implements Serializable {
+//	ScheduleModel eventModel = new DefaultScheduleModel();
+//	ScheduleEvent event = new DefaultScheduleEvent();
+//	
+//	  
+//	public ScheduleEvent getEvent() {
+//		return event;
+//	}
+//  
+//	public void setEvent(ScheduleEvent event) {
+//		this.event = event;
+//	}
+//	  
+//	public void addEvent(ActionEvent actionEvent) {
+//		if(event.getId() == null)
+//			eventModel.addEvent(event);
+//		else
+//			eventModel.updateEvent(event);
+//		  
+//		event = new DefaultScheduleEvent();
+//	}
+//	  
+//	public void onEventSelect(SelectEvent selectEvent) {
+//		event = (ScheduleEvent) selectEvent.getObject();
+//	}
+//	  
+//	public void onDateSelect(SelectEvent selectEvent) {
+//		event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+//	}
+//	  
+//	public void onEventMove(ScheduleEntryMoveEvent event) {
+//		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+//		  
+//		print message
+//	}
+//	  
+//	public void onEventResize(ScheduleEntryResizeEvent event) {
+//		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+//		  
+//		print message
+//	}
+//}
 
-import javax.faces.application.FacesMessage
-import javax.faces.context.FacesContext
-import javax.faces.event.ActionEvent
-import javax.inject.Named
 
-import org.primefaces.event.ScheduleEntryMoveEvent
-import org.primefaces.event.ScheduleEntryResizeEvent
-import org.primefaces.event.SelectEvent
-import org.primefaces.model.DefaultScheduleEvent
-import org.primefaces.model.DefaultScheduleModel
-import org.primefaces.model.ScheduleEvent
-import org.primefaces.model.ScheduleModel
-import org.springframework.context.annotation.Scope
-
-/**
- * 
- * @author Ács Ádám
- *
- */
-@org.springframework.stereotype.Component("scheduleBean")
-@Scope("session")
-class ScheduleBean implements Serializable {
-	ScheduleModel eventModel = new DefaultScheduleModel();
-	ScheduleEvent event = new DefaultScheduleEvent();
-	
-	  
-	public ScheduleEvent getEvent() {
-		return event;
-	}
-  
-	public void setEvent(ScheduleEvent event) {
-		this.event = event;
-	}
-	  
-	public void addEvent(ActionEvent actionEvent) {
-		if(event.getId() == null)
-			eventModel.addEvent(event);
-		else
-			eventModel.updateEvent(event);
-		  
-		event = new DefaultScheduleEvent();
-	}
-	  
-	public void onEventSelect(SelectEvent selectEvent) {
-		event = (ScheduleEvent) selectEvent.getObject();
-	}
-	  
-	public void onDateSelect(SelectEvent selectEvent) {
-		event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
-	}
-	  
-	public void onEventMove(ScheduleEntryMoveEvent event) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-		  
-		print message
-	}
-	  
-	public void onEventResize(ScheduleEntryResizeEvent event) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
-		  
-		print message
-	}
-}
